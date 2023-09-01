80111f046a39c7aeb10cf0fd7f637314f5482b78
Harshal151 <adityanivalkar41@gmail.com>
2023-09-01 11:23:01 +0530

    level 4

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-//jshint esversion:6
+// jshint esversion:6
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
-const app = express();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-mongoose.connect("mongodb://localhost:27017/userDB");
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const userSchema = new mongoose.Schema({
   mail: String,
   password: String,
 });
 
-
-// userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields : ["password"]});
-
-const User = new mongoose.model("User", userSchema);
-
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -36,49 +36,51 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/register", (req, res) => {
-  const newUser = new User({
-    mail: req.body.username,
-    password: md5(req.body.password),
-  });
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
-  newUser
-    .save()
-    .then(() => {
-      res.render("secrets");
-    })
-    .catch((err) => {
-      console.log(err);
+    const newUser = new User({
+      mail: req.body.username,
+      password: hashedPassword,
     });
+
+    await newUser.save();
+    res.render("secrets");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-app.post("/login", (req, res) => {
-    const username = req.body.username; 
-    const password = md5(req.body.password);
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-    User.findOne({ mail: username }) 
-        .exec()
-        .then((foundUser) => {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    console.log("Authentication successful!");
-                    res.render("secrets");
-                } else {
-                    console.log("Incorrect password!");
-                    res.status(401).send("Incorrect password");
-                }
-            } else {
-                console.log("User not found!");
-                res.status(404).send("User not found");
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send("Internal Server Error");
-        });
+  try {
+    const foundUser = await User.findOne({ mail: username }).exec();
+
+    if (foundUser) {
+      const isPasswordValid = await bcrypt.compare(password, foundUser.password);
+
+      if (isPasswordValid) {
+        console.log("Authentication successful!");
+        res.render("secrets");
+      } else {
+        console.log("Incorrect password!");
+        res.status(401).send("Incorrect password");
+      }
+    } else {
+      console.log("User not found!");
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-
-app.listen("3000", () => {
-  console.log("Port started at 3000");
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
